e713681924ff6590fc404f1abc3f8d3038f405d6
Valentin Vanelslande <vvanelslandedev@gmail.com>
2019-09-30 14:26:50 -0500

    Update pre-upload.sh

--- .travis/common/pre-upload.sh
+++ .travis/common/pre-upload.sh
@@ -5,6 +5,4 @@ export CITRA_VERSION=vvanelslande-$(python3 .travis/common/get-version.py)
 git config --local user.name travis-ci
 git config --local user.email none
 
-export TRAVIS_TAG=$CITRA_VERSION
-
 mkdir -p artifacts
