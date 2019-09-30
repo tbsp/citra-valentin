2d4f97e380fa80ab62d41bfbab22dc9d59faf9de
Valentin Vanelslande <vvanelslandedev@gmail.com>
2019-09-30 12:08:00 -0500

    Update get-version.py

--- .travis/common/get-version.py
+++ .travis/common/get-version.py
@@ -2,8 +2,6 @@ from re import search
 from subprocess import run
 from os.path import join, abspath
 
-print(abspath(join('src', 'common', 'version.h')))
-
 with open(abspath(join('src', 'common', 'version.h'))) as h:
     text = h.read()
     VERSION_RE = r'constexpr u16 TYPE = \b(.*)\b;'
@@ -13,6 +11,6 @@ with open(abspath(join('src', 'common', 'version.h'))) as h:
     patch = None
 
     for t in ['major', 'minor', 'patch']:
-        exec(f'{t} = {search(VERSION_RE.replace("TYPE", t), text)[1]}')
+        exec('{} = {}'.format(t, search(VERSION_RE.replace("TYPE", t), text)[1]))
 
     print(f'{major}.{minor}.{patch}')
