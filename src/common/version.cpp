95e7d825dd0ba8b031f21aec1c61a5147b5068eb
Valentin Vanelslande <vvanelslandedev@gmail.com>
2019-09-30 17:08:04 -0500

    Update version.cpp

--- src/common/version.cpp
+++ src/common/version.cpp
@@ -2,12 +2,10 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#pragma once
-
 #include "common/version.h"
 
 namespace Version {
 const u16 major = 1;
 const u16 minor = 0;
-const u16 patch = 5;
+const u16 patch = 6;
 } // namespace Version
