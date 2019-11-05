5ff255b84a606716cd7b27ddbdb44cd07a50c318
vvanelslande <vvanelslandedev@gmail.com>
2019-11-05 02:20:06 -0500

    asasdsadad

--- .travis/windows/build.ps1
+++ .travis/windows/build.ps1
@@ -2,4 +2,4 @@ mkdir build
 Set-Location build
 
 cmake -DCITRA_ENABLE_DISCORD_RP=ON -DENABLE_FFMPEG_VIDEO_DUMPER=ON ..
-cmake --build . --target citra-qt --config Release
+cmake --build . --target citra-qt --config Release -- /property:GenerateFullPaths=true
