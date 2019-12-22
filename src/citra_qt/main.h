f906d5283ef04fb9d113a38fc7f56ea7df637ba1
vvanelslande <vvanelslandedev@gmail.com>
2019-12-21 22:17:53 -0500

    add newlines

--- src/citra_qt/main.h
+++ src/citra_qt/main.h
@@ -180,8 +180,10 @@ private:
     // Whether emulation is currently running in Citra.
     bool emulation_running = false;
     std::unique_ptr<EmuThread> emu_thread;
+
     // The title of the game currently running
     QString game_title;
+
     // The path to the game currently running
     QString game_path;
 
